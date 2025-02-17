9d7ab083a778a79848057665a1943a0b9133527c
Dyrgyzark <nick@cpnkirk.com>
2025-02-16 19:21:44 -0600

    idk man mostly optimizations and stuff

--- src/main/java/com/mycompany/uttttest/Fen.java
+++ src/main/java/com/mycompany/uttttest/Fen.java
@@ -4,9 +4,8 @@
  */
 package com.mycompany.uttttest;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,19 +19,36 @@ public class Fen {
     private boolean activePlayer;
     private int activeBoard;
     private final List<String> boardList;
+    private final Boolean[][] moveArrayArray = new Boolean[9][9];
     
     /**
      * Initializes Fen class with string input for fen notation
-     * should probably add a default Fen that it the starting board
+     * should probably add a default Fen that is the starting board
      * @param fenInput
      */
     public Fen(String fenInput)
     {
         List<String> fenInputs = split(fenInput, " ");
+        
         placementData = fenInputs.get(0);
+        
         activePlayer = fenInputs.get(1).equals("x");
-        activeBoard = Integer.parseInt(fenInputs.get(2));
+        
+        try
+        {
+            activeBoard = Integer.parseInt(fenInputs.get(2)) - 1;
+        }
+        catch (NumberFormatException NumberFormatException)
+        {
+            activeBoard = -1;
+        }
+        
         boardList = split(fenInputs.get(0), "/");
+        
+        for (int i = 0; i < 9; ++i)
+        {
+            moveArrayArray[i] = getMoveArray(boardList.get(i));
+        }
         
         
     }
@@ -54,11 +70,11 @@ public class Fen {
         int arrIndex = 0;
         for (int i = 0; i < boardState.length(); ++i)
         {
-            try
+            if (boardState.charAt(i) >= '0' && boardState.charAt(i) <= '9')
             {
-                arrIndex += Integer.parseInt(boardState.substring(i,i + 1));
+                arrIndex += boardState.charAt(i) - '0';
             } 
-            catch(NumberFormatException NumberFormatException)
+            else
             {
                 // sets to true if x, false if o
                 boardArray[arrIndex] = boardState.substring(i,i + 1).equals("x");
@@ -106,13 +122,82 @@ public class Fen {
         return(boardState);
     }
     
+    // returns 1 if x won
+    // returns 2 if o won
+    // returns 0 if board is tied
+    // returns -1 if board is not finished
+    private int boardIsWon(Boolean[] moveArray)
+    {
+        int boardState = -1;
+        
+        // testing if board is full
+        if (
+                (moveArray[0] != null && moveArray[1] != null && moveArray[2] != null && 
+                moveArray[3] != null && moveArray[4] != null && moveArray[5] != null && 
+                moveArray[6] != null && moveArray[7] != null && moveArray[8] != null)
+            )
+        {
+            boardState = 0;
+        }
+        
+        // testing if there is a win on the diagonals
+        // 0, 4, 8 is the main diagonal
+        if (moveArray[0] != null && moveArray[0] == moveArray[4] && moveArray[4] == moveArray[8])
+        {
+            boardState = moveArray[0] == true ? 1 : 2;
+        }
+        // 2, 4, 6 is the Anti-diagonal
+        if (moveArray[2] != null && moveArray[2] == moveArray[4] && moveArray[4] == moveArray[6])
+        {
+            boardState = moveArray[2] == true ? 1 : 2;
+        }
+        
+        // checks for each orthogonal win
+        for (int i = 0; i < 3; ++i)
+            {
+                if (moveArray[3*i] != null &&
+                    moveArray[3*i] == moveArray[3*i+1] &&
+                    moveArray[3*i+1] == moveArray[3*i+2])
+                {
+                    boardState = moveArray[3*i] ? 1 : 2;
+                }
+                if (moveArray[i] != null &&
+                    moveArray[i] == moveArray[i+3] &&
+                    moveArray[i+3] == moveArray[i+6])
+                {
+                    boardState = moveArray[i] ? 1 : 2;
+                }
+            }
+        
+        return(boardState);
+    }
+    
+    private Boolean winner()
+    {
+        Boolean[] boardArray = new Boolean[9];
+        for (int i = 0; i < 9; ++i)
+        {
+            boardArray[i] = switch(boardIsWon(moveArrayArray[i]))
+            {
+                case 1 -> true;
+                case 2 -> false;
+                default -> null;
+            };
+        }
+        return(switch(boardIsWon(boardArray))
+        {
+            case 1 -> true;
+            case 2 -> false;
+            default -> null;
+        });
+    }
+    
     // checks if a move (majPos, minPos) is able to be played
     // given the current board state fen
     // if minPos = -1, we're testing if you can play anywhere in the majPos board
     private boolean validateMove(int majPos, int minPos)
     {
-        String boardState = boardList.get(majPos);
-        Boolean[] moveArray = getMoveArray(boardState);
+        Boolean[] moveArray = moveArrayArray[majPos];
         boolean valid = true;
         // testing if player can currently play in majPos board 
         // and if there's already a move played there
@@ -121,42 +206,24 @@ public class Fen {
             valid = ((activeBoard < 0) || (majPos == activeBoard)) && (moveArray[minPos] == null);
         }
         
-        // testing if there is a win on the diagonals and if the board is full
-        // 0, 4, 8 is the main diagonal
-        // 2, 4, 6 is the Anti-diagonal
-        if (
-                (Objects.equals(moveArray[0], moveArray[4]) && Objects.equals(moveArray[4], moveArray[8])) && !Objects.equals(moveArray[0], null) ||
-                (Objects.equals(moveArray[2], moveArray[4]) && Objects.equals(moveArray[4], moveArray[6])) && !Objects.equals(moveArray[2], null) ||
-                (moveArray[0] != null && moveArray[1] != null && moveArray[2] != null && 
-                moveArray[3] != null && moveArray[4] != null && moveArray[5] != null && 
-                moveArray[6] != null && moveArray[7] != null && moveArray[8] != null)
-            )
+       
+        if (boardIsWon(moveArray) >= 0)
         {
             valid = false;
         }
-        // checks for each orthogonal win
-        for (int i = 0; i < 3 && valid; ++i)
-            {
-                if ( 
-                        (Objects.equals(moveArray[3*i], moveArray[3*i+1]) && Objects.equals(moveArray[3*i+1], moveArray[3*i+2])) && !Objects.equals(moveArray[3*i], null) ||
-                        (Objects.equals(moveArray[i], moveArray[i+3]) && Objects.equals(moveArray[i+3], moveArray[i+6])) && !Objects.equals(moveArray[i], null)           
-                    )
-                {
-                    valid = false;
-                }
-            }
-        return valid;
+        
+        Boolean winner = winner();
+        return(!(winner != null) && valid);
     }
     
     public void addMove(int majPos, int minPos)
     {
-        minPos--; majPos--;
-        String boardState = boardList.get(majPos);
-        Boolean[] moveArray = getMoveArray(boardState);
+        Boolean[] moveArray = moveArrayArray[majPos];
         if (validateMove(majPos, minPos))
         {
             moveArray[minPos] = activePlayer;
             boardList.set(majPos, moveArrayToBoardState(moveArray));
+            moveArrayArray[majPos] = moveArray;
             placementData = String.join("/", boardList);
             activePlayer = !activePlayer;
             activeBoard = minPos;
@@ -188,15 +255,47 @@ public class Fen {
         return fen;
     }
     
+    public int getActiveBoard()
+    {
+        return(activeBoard);
+    }
+    
+    public List<int[]> getMoveList()
+    {
+        List<int[]> moveList = new ArrayList<>();
+        int[] move = new int[2];
+        if (activeBoard < 0)
+        {
+            for (int i = 0; i < 9; ++i)
+            {
+                for (int j = 0; j < 9; ++j)
+                {
+                    if (validateMove(i, j))
+                    {
+                        move[0] = i;
+                        move[1] = j;
+                        moveList.add(move.clone());
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 9; ++i)
+            {
+                if (validateMove(activeBoard, i))
+                {
+                    move[0] = activeBoard;
+                    move[1] = i;
+                    moveList.add(move.clone());
+                }
+            }
+        }
+        return(moveList);
+    }
+    
     public void printPrettyFen()
     {
-        String[] fullBoardState = new String[9];
-        Boolean[][] fullMoveArray = new Boolean[9][9];
-        for (int i = 0; i < 9; ++i)
-        {
-            fullBoardState[i] = boardList.get(i);
-            fullMoveArray[i] = getMoveArray(fullBoardState[i]);
-        }
         
         String prettyFen = "";
         for (int i = 0; i < 9; ++i)
@@ -212,9 +311,9 @@ public class Fen {
             {
                 prettyFen += "  ";
             }
-                if (fullMoveArray[j/3+3*(i/3)][j%3 + (3*i)%9] != null)
+                if (moveArrayArray[j/3+3*(i/3)][j%3 + (3*i)%9] != null)
                 {
-                    if (fullMoveArray[j/3+3*(i/3)][j%3 + (3*i)%9])
+                    if (moveArrayArray[j/3+3*(i/3)][j%3 + (3*i)%9])
                     {
                         prettyFen += "x ";
                     }
@@ -232,6 +331,6 @@ public class Fen {
             prettyFen += "  \n";
             
         }
-                    System.out.println(prettyFen);
+        System.out.println(prettyFen);
     }
 }
--- src/main/java/com/mycompany/uttttest/UTTTTest.java
+++ src/main/java/com/mycompany/uttttest/UTTTTest.java
@@ -4,15 +4,59 @@
 
 package com.mycompany.uttttest;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 public class UTTTTest {
     static Scanner sc = new Scanner(System.in);
+    
+    private static int[] genRandomMove(Fen fen)
+    {
+        Random rand = new Random();
+        List<int[]> moveList = fen.getMoveList();
+        return(moveList.get(rand.nextInt(0, moveList.size())));
+    }
+    
+    private static void playRandomGame(String[] game)
+    {
+        int[] move;
+        Fen fen = new Fen("9/9/9/9/9/9/9/9/9 x -");
+            while (!fen.getMoveList().isEmpty())
+            {
+               move = genRandomMove(fen);
+                fen.addMove(move[0], move[1]);
+                game[0] += Arrays.toString(move) + " ";
+            }
+    }
 
-    public static void main(String[] args) {
-        Fen fen = new Fen("9/9/9/9/9/9/9/9/9 x -1");
-        while (true) {
-            int[] moves = {Integer.parseInt(sc.nextLine()), Integer.parseInt(sc.nextLine())};
-            fen.addMove(moves[0], moves[1]);
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] game = new String[1];
+//        System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("Games.txt")), true));
+//        for (int i = 0; i < 10000000; ++i)
+//        {
+//            game[0] = "";
+//            playRandomGame(game);
+//            System.out.println(game[0] + "\n");
+//        }
+//        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+//        while (true)
+//        {
+//            String inputFen = sc.nextLine();
+//            Fen fen = new Fen(inputFen);
+//            fen.printPrettyFen();
+//        }
+
+        int[] moves = new int[2];
+        Fen fen = new Fen("9/9/9/9/9/9/9/9/9 x -");
+        System.out.println(fen.getFen());
+        fen.printPrettyFen();
+        while (true)
+        {
+            moves[0] = Integer.parseInt(sc.nextLine());
+            moves[1] = Integer.parseInt(sc.nextLine());
+            fen.addMove(moves[0] - 1, moves[1] - 1);
             System.out.println(fen.getFen());
             fen.printPrettyFen();
         }
